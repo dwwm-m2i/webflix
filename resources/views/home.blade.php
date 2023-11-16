48030f74d91281ed987d14223eeec4b5aa6add6a
Matthieu Mota <matthieumota@gmail.com>
2023-11-16 11:06:47 +0100

    Blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello {{ $name }}</h1>
+
+    <ul>
+    @foreach ($games as $game)
+        <li>{{ $game }}</li>
+    @endforeach
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'name' => 'Fiorella',
+        'games' => [
+            'Elden Ring',
+            'Call of Duty',
+            'FC 24',
+        ],
+    ]);
 });
