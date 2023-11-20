094361cc43ee33a1de59a30eaabbdb9428f9b561
Matthieu Mota <matthieumota@gmail.com>
2023-11-20 14:43:20 +0100

    Seeder from api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Movie;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,15 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory(9)->create();
-        Category::factory()->create(['name' => 'Action']);
+        // SANS API
+        // Category::factory(9)->create();
+        // Category::factory()->create(['name' => 'Action']);
 
-        Movie::factory(100)->create(function () {
+        // Movie::factory(100)->create(function () {
             // Ici c'est comme une boucle
-            return [
-                'category_id' => rand(1, 10),
-            ];
-        });
+        //     return [
+        //         'category_id' => rand(1, 10),
+        //     ];
+        // });
+
+        // Avec API
+        $apiKey = '1acc688a333a713673ba5711f8f671d0';
+        $baseUrl = 'https://api.themoviedb.org/3';
+
+        // Catégories
+        // On fait une requête sur une API
+        // withoutVerifying() -> Désactiver le HTTPS sous Windows
+        $genres = Http::withoutVerifying()
+            ->get($baseUrl.'/genre/movie/list?language=fr-FR&api_key='.$apiKey)->json('genres');
+
+        Category::factory()->createMany($genres);
+
+        // Films
 
         // \App\Models\User::factory(10)->create();
 
