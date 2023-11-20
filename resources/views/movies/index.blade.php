d5ff29524af5f1f55aa4b59001c44590fbe13ec2
Matthieu Mota <matthieumota@gmail.com>
2023-11-20 16:55:37 +0100

    Front

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,20 +6,22 @@
         <a href="/films/creer" class="px-4 py-2 text-sm bg-blue-500 hover:bg-blue-300 duration-200 text-white rounded-full shadow-sm">Créer un film</a>
     </div>
 
-    <div>
+    <div class="flex flex-wrap -mx-3">
         @foreach ($movies as $movie)
-            <div>
-                <h3>{{ $movie->title }}</h3>
-                <p>{{ $movie->synopsis }}</p>
-                <p>Durée: {{ $movie->duration }}</p>
-                <img class="w-32" src="{{ $movie->cover }}" alt="{{ $movie->title }}">
-                @if ($movie->released_at)
-                <p>Sortie: {{ $movie->released_at }}</p>
-                @endif
-                @if ($movie->category_id)
-                <p>Catégorie: {{ $movie->category_id }}</p>
-                @endif
-                <a href="/film/{{ $movie->id }}">Voir</a>
+            <div class="w-1/2 md:w-1/3 lg:w-1/5">
+                <a href="/film/{{ $movie->id }}" class="block mx-3 group">
+                    <img class="w-full h-[300px] object-cover mb-3" src="{{ $movie->cover }}" alt="{{ $movie->title }}">
+                    <h3 class="text-sm text-gray-600 underline group-hover:no-underline">{{ $movie->title }}</h3>
+                    <p class="text-sm mb-3">
+                        @if ($movie->released_at)
+                        {{ $movie->released_at }} |
+                        @endif
+                        @if ($movie->category_id)
+                        {{ $movie->category_id }} |
+                        @endif
+                        {{ $movie->duration }}
+                    </p>
+                </a>
             </div>
         @endforeach
     </div>
