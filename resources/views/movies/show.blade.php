75dd4501f5c40b756a1ce3dc9d53f40a055bf9e5
Matthieu Mota <matthieumota@gmail.com>
2023-11-21 10:33:22 +0100

    Front movie

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,17 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <a href="/films" class="text-gray-500">Retour aux films</a>
-        <h1>{{ $movie->title }}</h1>
-        <p>{{ $movie->synopsis }}</p>
-        <p>Durée: {{ $movie->duration }}</p>
-        <img class="w-32" src="{{ $movie->cover }}" alt="{{ $movie->title }}">
-        @if ($movie->released_at)
-        <p>Sortie: {{ $movie->released_at }}</p>
-        @endif
-        @if ($movie->category_id)
-        <p>Catégorie: {{ $movie->category_id }}</p>
-        @endif
+    <a href="/films" class="text-gray-500">Retour aux films</a>
+    <div class="flex gap-10 items-center">
+        <div class="w-2/5">
+            <img class="w-full" src="{{ $movie->cover }}" alt="{{ $movie->title }}">
+        </div>
+        <div class="w-3/5">
+            <div class="border p-4 shadow-lg">
+                <h1 class="text-3xl">{{ $movie->title }}</h1>
+                <p class="my-3">{{ $movie->synopsis }}</p>
+                <p class="text-sm">
+                    @if ($movie->released_at)
+                    {{ $movie->released_at->diffForHumans() }} |
+                    {{ $movie->released_at->translatedFormat('d F Y') }} |
+                    @endif
+                    @if ($movie->category_id)
+                    {{ $movie->category->name }} |
+                    @endif
+                    {{ $movie->duration() }}
+                </p>
+            </div>
+        </div>
     </div>
 @endsection
